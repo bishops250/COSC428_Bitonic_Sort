a6b29cef6c5e831f821679446fccd42df20c7905
ab <bishops250@gmail.com>
2022-12-05 13:13:11 -0500

    changes

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ int main()
 	int arr[8] = {3, 7, 4, 8, 6, 2, 1, 5};
 
 	// cout<<N<<"\n";
-	bitonicSort(arr, 8, 1, -1);
+	bitonicSort(arr, 8, 1, 1);
 
 	for (int i = 0; i < 8; i++)
 	{
 		cout << arr[i] << " ";
 	}
+	cout << endl;
 	return 0;
 }
